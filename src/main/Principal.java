4e39fc0d8cf62d4be35c9ceee97bf18ad98ef9f9
Hugo Fernández <hugo.f@opendeusto.es>
2025-10-15 23:11:31 +0200

    Commit prueba clases

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -2,6 +2,7 @@ package main;
 
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Prueba commit");
+		System.out.println("Prueba commit - Hugo Fernández");
+		System.out.println("Prueba commit - Portatil Hugo Fernández");
 	}
 }
