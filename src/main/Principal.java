58c994826c4907adfb19b13105836e2dcd9a9149
Pablo Hierro <pablo.hierro@opendeusto.es>
2025-10-16 11:06:26 +0200

    Commit Pablo

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
 		System.out.println("Prueba commit - Hugo Fernández");
 		System.out.println("Prueba commit - Portatil Hugo Fernández");
 		System.out.println("Prueba commit - Beñat Arrieta");
+		System.out.println("Commit Pablo Hierro");
 	}
 }
