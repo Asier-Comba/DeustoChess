354b4cfb73afd4926d8b412ad0ff1611b38edf50
Beñat Arrieta <benat.arrieta@opendeusto.es>
2025-10-16 10:07:06 +0200

    Commit

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Prueba commit - Hugo Fernández");
 		System.out.println("Prueba commit - Portatil Hugo Fernández");
+		System.out.println("Prueba commit - Beñat Arrieta");
 	}
 }
