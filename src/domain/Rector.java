ab9a44cc3fa2fad9f48597f365122efa0a0199ab
asier <asier@asier-desktop>
2025-10-17 20:41:19 +0200

    He creado las clases Expediente, Expulsion, HabilidadEspecial y Rector, he borrado las clases del ajedrez antiguo y ademas he programado la clase pieza y la clase rector

---
+++ src/domain/Rector.java
@@ -0,0 +1,60 @@
+package domain;
+
+public class Rector extends Pieza{
+	protected boolean haUsadoHabilidad;
+	protected boolean expediente;
+	protected boolean expulsion;
+	
+	public Rector(String nombre, Movimiento movimiento, HabilidadEspecial habilidad, String color, int fila,
+			int columna, boolean haUsadoHabilidad, boolean expediente, boolean expulsion) {
+		super(nombre, movimiento, habilidad, color, fila, columna);
+		this.haUsadoHabilidad = haUsadoHabilidad;
+		this.expediente = expediente;
+		this.expulsion = expulsion;
+	}
+
+	public boolean isHaUsadoHabilidad() {
+		return haUsadoHabilidad;
+	}
+
+	public void setHaUsadoHabilidad(boolean haUsadoHabilidad) {
+		this.haUsadoHabilidad = haUsadoHabilidad;
+	}
+
+	public boolean isExpediente() {
+		return expediente;
+	}
+
+	public void setExpediente(boolean expediente) {
+		this.expediente = expediente;
+	}
+
+	public boolean isExpulsion() {
+		return expulsion;
+	}
+
+	public void setExpulsion(boolean expulsion) {
+		this.expulsion = expulsion;
+	}
+
+	@Override
+	public String toString() {
+		return "Rector [haUsadoHabilidad=" + haUsadoHabilidad + ", expediente=" + expediente + ", expulsion="
+				+ expulsion + "]";
+	}
+
+	@Override
+	public boolean movimientoValido(int nuevaFila, int nuevaColumna, Tablero tablero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void usarHabilidad(Tablero tablero) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
