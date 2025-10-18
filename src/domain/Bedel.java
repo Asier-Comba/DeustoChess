79a451d6e129b6de09e123cb7484b9873f94560a
Pablo Hierro <pablo.hierro@opendeusto.es>
2025-10-18 23:56:37 +0200

    He creado las clases Alumno, Becario, Bedel, MaquinaExpendedora y Seretaria.

---
+++ src/domain/Bedel.java
@@ -0,0 +1,55 @@
+package domain;
+
+public class Bedel extends Pieza{
+	protected boolean haUsadoHabilidad;
+	protected boolean expediente;
+	protected boolean expulsion;
+	
+	public Bedel(String nombre, Movimiento movimiento, HabilidadEspecial habilidad, String color, int fila,
+			int columna, boolean haUsadoHabilidad, boolean expediente, boolean expulsion) {
+		super(nombre, movimiento, habilidad, color, fila, columna);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public boolean isHaUsadoHabilidad() {
+		return haUsadoHabilidad;
+	}
+
+
+	public void setHaUsadoHabilidad(boolean haUsadoHabilidad) {
+		this.haUsadoHabilidad = haUsadoHabilidad;
+	}
+
+
+	public boolean isExpediente() {
+		return expediente;
+	}
+
+
+	public void setExpediente(boolean expediente) {
+		this.expediente = expediente;
+	}
+
+
+	public boolean isExpulsion() {
+		return expulsion;
+	}
+
+
+	public void setExpulsion(boolean expulsion) {
+		this.expulsion = expulsion;
+	}
+
+
+	@Override
+	public boolean movimientoValido(int nuevaFila, int nuevaColumna, Tablero tablero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void usarHabilidad(Tablero tablero) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/domain/Secretaria.java
@@ -0,0 +1,56 @@
+package domain;
+
+public class Secretaria extends Pieza {
+	protected boolean haUsadoHabilidad;
+	protected boolean expediente;
+	protected boolean expulsion;
+	
+	public Secretaria(String nombre, Movimiento movimiento, HabilidadEspecial habilidad, String color, int fila,
+			int columna, boolean haUsadoHabilidad, boolean expediente, boolean expulsion) {
+		super(nombre, movimiento, habilidad, color, fila, columna);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public boolean isHaUsadoHabilidad() {
+		return haUsadoHabilidad;
+	}
+
+
+	public void setHaUsadoHabilidad(boolean haUsadoHabilidad) {
+		this.haUsadoHabilidad = haUsadoHabilidad;
+	}
+
+
+	public boolean isExpediente() {
+		return expediente;
+	}
+
+
+	public void setExpediente(boolean expediente) {
+		this.expediente = expediente;
+	}
+
+
+	public boolean isExpulsion() {
+		return expulsion;
+	}
+
+
+	public void setExpulsion(boolean expulsion) {
+		this.expulsion = expulsion;
+	}
+
+
+	@Override
+	public boolean movimientoValido(int nuevaFila, int nuevaColumna, Tablero tablero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void usarHabilidad(Tablero tablero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
