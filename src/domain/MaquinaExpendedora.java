7c52543861c4ba50a682d178e39759d6d82cb510
Beñat Arrieta <benat.arrieta@opendeusto.es>
2025-10-19 18:43:08 +0200

    He puesto los getters y setters y así de la maquina expendedora

--- src/domain/MaquinaExpendedora.java
+++ src/domain/MaquinaExpendedora.java
@@ -1,5 +1,39 @@
 package domain;
 
-public class MaquinaExpendedora {
+public class MaquinaExpendedora extends Pieza{
+	protected boolean haUsadoHabilidad;
+	
+	public MaquinaExpendedora(String nombre, Movimiento movimiento, HabilidadEspecial habilidad, String color, int fila,
+			int columna, boolean haUsadoHabilidad) {
+		super(nombre, movimiento, habilidad, color, fila, columna);
+		this.haUsadoHabilidad = haUsadoHabilidad;
+
+	}
+
+	public boolean isHaUsadoHabilidad() {
+		return haUsadoHabilidad;
+	}
+
+	public void setHaUsadoHabilidad(boolean haUsadoHabilidad) {
+		this.haUsadoHabilidad = haUsadoHabilidad;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "MaquinaExpendedora [haUsadoHabilidad=" + haUsadoHabilidad + "]";
+	}
+
+	@Override
+	public boolean movimientoValido(int nuevaFila, int nuevaColumna, Tablero tablero) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void usarHabilidad(Tablero tablero) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
